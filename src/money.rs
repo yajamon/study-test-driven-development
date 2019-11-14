344ef885cbf529410adaa4fb6565272b10b07b13
yajamon <yajamon.tatsuki@gmail.com>
2019-11-14 22:20:02 +0900

    Red: Dollar.times()に副作用があってほしくない

--- src/money.rs
+++ src/money.rs
@@ -19,6 +19,8 @@ mod test {
     fn test_multiplication() {
         let mut five = Dollar::new(5);
         five.times(2);
-        assert_eq!(10, five.amount)
+        assert_eq!(10, five.amount);
+        five.times(3);
+        assert_eq!(15, five.amount);
     }
 }
