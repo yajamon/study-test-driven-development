ef6113be5e45c8375bfb9b99e7d2ba489b376ff6
yajamon <yajamon.tatsuki@gmail.com>
2019-11-14 21:28:56 +0900

    テストを修正

--- src/money.rs
+++ src/money.rs
@@ -2,7 +2,7 @@
 mod test {
     #[test]
     fn test_multiplication() {
-        let mut five = Doller { amount: 5 };
+        let mut five = Dollar::new(5);
         five.times(2);
         assert_eq!(10, five.amount)
     }
