776ee5e469f99653f9c8b5bdbd222d9e1b197828
yajamon <yajamon.tatsuki@gmail.com>
2019-11-20 03:16:27 +0900

    可視性の漏れを修正
    Fix #17,#12

--- src/money.rs
+++ src/money.rs
@@ -8,16 +8,16 @@ impl Money {
     fn new(amount: i64, currency: String) -> Money {
         Money { amount, currency }
     }
-    fn dollar(amount: i64) -> Money {
+    pub fn dollar(amount: i64) -> Money {
         Money::new(amount, "USD".to_string())
     }
-    fn franc(amount: i64) -> Money {
+    pub fn franc(amount: i64) -> Money {
         Money::new(amount, "CHF".to_string())
     }
-    fn times(&self, multiplier: i64) -> Self {
+    pub fn times(&self, multiplier: i64) -> Self {
         Money::new(self.amount * multiplier, self.currency.to_string())
     }
-    fn currency(&self) -> &str {
+    pub fn currency(&self) -> &str {
         &self.currency
     }
 }
