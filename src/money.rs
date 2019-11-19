cbfb3c6a640017e1e079f3c0d5232612f2735ae6
yajamon <yajamon.tatsuki@gmail.com>
2019-11-20 03:11:02 +0900

    Refactoring: Moneyをenumからstructへ変更
    Dollar,Francヴァリアントが消えた。
    いまやMoneyがenumである理由はなくなった。

--- src/money.rs
+++ src/money.rs
@@ -1,12 +1,12 @@
 #[derive(Debug)]
-pub enum Money {
-    // amount, currency
-    Money(i64, String),
+pub struct Money {
+    amount: i64,
+    currency: String,
 }
 
 impl Money {
     fn new(amount: i64, currency: String) -> Money {
-        Money::Money(amount, currency)
+        Money { amount, currency }
     }
     fn dollar(amount: i64) -> Money {
         Money::new(amount, "USD".to_string())
@@ -15,24 +15,17 @@ impl Money {
         Money::new(amount, "CHF".to_string())
     }
     fn times(&self, multiplier: i64) -> Self {
-        match self {
-            Money::Money(amount, currency) => Money::new(amount * multiplier, currency.to_string()),
-        }
+        Money::new(self.amount * multiplier, self.currency.to_string())
     }
     fn currency(&self) -> &str {
-        match self {
-            Money::Money(_, currency) => currency,
-        }
+        &self.currency
     }
 }
 impl PartialEq for Money {
     fn eq(&self, other: &Self) -> bool {
-        let left = match self {
-            Money::Money(amount, currency) => (amount, currency),
-        };
-        let right = match other {
-            Money::Money(amount, currency) => (amount, currency),
-        };
+        let left = (self.amount, &self.currency);
+        let right = (other.amount, &other.currency);
+
         left == right
     }
 }
